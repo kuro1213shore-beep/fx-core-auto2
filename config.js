574d79eaf57b3ceb097e4a41b33cd8ced286d625
kuro1213shore-beep <kuro1213shore@icloud.com>
2026-02-11 16:18:52 +0900

    config.js を作成

---
+++ config.js
@@ -0,0 +1,12 @@
+export const CONFIG = {
+  trend: {
+    maMultiplier: 1.002,
+    riskThreshold: 2,
+    scoreEntry: 1
+  },
+  range: {
+    maMultiplier: 1.001,
+    riskThreshold: 1,
+    scoreEntry: 0
+  }
+};
