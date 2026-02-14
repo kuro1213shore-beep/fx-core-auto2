659709f1d96a7089f9b37d2399b8c0c6e56cff8a
kuro1213shore-beep <kuro1213shore@icloud.com>
2026-02-14 11:17:48 +0900

    mode.js を更新

--- js/mode.js
+++ js/mode.js
@@ -1,1 +1,13 @@
-1
+export function detectMode(data){
+
+  // 4H 200SMA判定はまだ仮
+  // 今は totalScore を基準にする
+
+  let mode = "RANGE";
+
+  if (data.totalScore >= 3 || data.totalScore <= -3){
+    mode = "TREND";
+  }
+
+  return mode;
+}
