d139fecf36bd7cdc159694a6000b92080ab914bd
kuro1213shore-beep <kuro1213shore@icloud.com>
2026-02-14 20:06:03 +0900

    viewer.js を更新

--- js/logs/viewer.js
+++ js/logs/viewer.js
@@ -1,1 +1,30 @@
-1
+import { getLogs } from "./storage.js";
+
+export function showLogs(){
+
+  const logs = getLogs();
+
+  if(!logs.length){
+    alert("No logs yet");
+    return;
+  }
+
+  let text = "";
+
+  logs.forEach(l => {
+    text += `
+${l.date}
+${l.session} | ${l.mode}
+
+ENV: ${l.env} / DIR: ${l.dir}
+ORDER: ${l.order}
+
+Score: ${l.totalScore}
+Result: ${l.resultPips} pips
+${l.comment || ""}
+----------------
+`;
+  });
+
+  alert(text);
+}
